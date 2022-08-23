404e689f4d8fa803bac108dcd5a7c2664ee447f9
Antarip chatterjee <85601268+AntaripChat@users.noreply.github.com>
2022-08-23 20:27:02 +0530

    Create ML.py

---
+++ ML.py
@@ -0,0 +1,19 @@
+import numpy as nm  
+import matplotlib.pyplot as mtp  
+import pandas as pd  
+  
+#importing datasets  
+data_set= pd.read_csv('user_data.csv')  
+  
+#Extracting Independent and dependent Variable  
+x= data_set.iloc[:, [2,3]].values  
+y= data_set.iloc[:, 4].values  
+  
+# Splitting the dataset into training and test set.  
+from sklearn.model_selection import train_test_split  
+x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.25, random_state=0)  
+#feature Scaling  
+from sklearn.preprocessing import StandardScaler    
+st_x= StandardScaler()    
+x_train= st_x.fit_transform(x_train)    
+x_test= st_x.transform(x_test)       
